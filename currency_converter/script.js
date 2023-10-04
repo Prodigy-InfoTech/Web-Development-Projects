74ca9968ae30a5b2f8e9d00ac5be128abaf49e3e
Ishan Ahmad <nahidboss96@gmail.com>
2023-10-04 16:19:01 +0600

    added necessary comment on the code

--- currency_converter/script.js
+++ currency_converter/script.js
@@ -1,16 +1,23 @@
 
+// Select elements by class and ID
 var select = document.querySelectorAll('.currency'),
 
 input_currency = document.getElementById('input_currency'),
 
 output_currency = document.getElementById('output_currency');
 
+// API host
 const host = 'api.frankfurter.app';
+
+// Fetch currency data
 fetch(`https://${host}/currencies`)
   .then((data) => data.json())
   .then((data) => {
+
+    // Convert currency data to an array of entries
     const entries = Object.entries(data)
-    // console.log(entries)
+
+    // Loop through entries and populate select dropdowns
     for(i = 0; i < entries.length; i++)
     {
         select[0].innerHTML += `<option value = "${entries[i][0]}"> ${entries[i][0]} </option>`
@@ -18,19 +25,24 @@ fetch(`https://${host}/currencies`)
     }
   });
 
-  function converter(){
-    var input_currency_val = input_currency.value;
 
-    if(select[0].value != select[1].value){
-        const host = 'api.frankfurter.app';
-        fetch(`https://${host}/latest?amount=${input_currency_val}&from=${select[0].value}&to=${select[1].value}`)
-        .then((val) => val.json())
-        .then((val) => {
-            // alert(Object.values(val.rates)[0])
-            output_currency.value = Object.values(val.rates)[0];
-        });
-    }
-    else{
-        alert("please select two different currency")
-    }
+// Function to perform currency conversion
+function converter(){
+  var input_currency_val = input_currency.value;
+
+  if(select[0].value != select[1].value){
+    const host = 'api.frankfurter.app';
+
+    // Fetch the latest exchange rate data
+    fetch(`https://${host}/latest?amount=${input_currency_val}&from=${select[0].value}&to=${select[1].value}`)
+    .then((val) => val.json())
+    .then((val) => {
+      // Set the output currency value to the converted value
+      output_currency.value = Object.values(val.rates)[0];
+    });
   }
+  else{
+    // Alert the user to select two different currencies
+    alert("please select two different currency")
+  }
+}

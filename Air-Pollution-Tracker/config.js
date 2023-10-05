19b78b62c588a1ab551a72af397cd0f067a3e8f5
felixop7 <roshansahani226@gmail.com>
2023-10-05 09:57:02 +0545

    air pollution tracker

---
+++ Air-Pollution-Tracker/config.js
@@ -0,0 +1,6 @@
+var config = {
+    API_KEY:'02a34ede-d0bd-40b8-97fb-0681bff883e7',
+    API_ID:"071ccf8472dd414295501457983f1d4c",
+    API_VAL:'1a156ec9e8394462857fba2406a969b9'
+}
+
---
+++ Air-Pollution-Tracker/history_config.js
@@ -0,0 +1,3 @@
+var history_config = {
+    API_VAL:'1a156ec9e8394462857fba2406a969b9'
+}

c3b7fd6f29df403f2ea46d1aebac95282e6a7838
NilanjanPramanik <121485018+NilanjanPramanik@users.noreply.github.com>
2023-10-08 17:28:40 +0530

    Feature: Added Weather App using React js and openweathermap api (#66)
    * Added Weather App in ReactJS
    
    * Update App.js
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md

---
+++ Weather App/src/LoadingComponent.js
@@ -0,0 +1,9 @@
+function Loading() {
+    return (
+        <div className="flex items-center justify-center">
+            <div className="animate-spin rounded-full h-7 w-7 border-t-2 border-solid ml-1"></div>
+        </div>
+    );
+}
+
+export default Loading;

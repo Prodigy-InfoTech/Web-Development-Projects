c16019233279fc903913d43121ed3a7b15c815be
poudyalamit <poudyalamit20@gmail.com>
2023-10-27 07:14:26 +0545

    removed 3D-Models

--- 3D-Models/client/src/components/index.js
+++
@@ -1,9 +0,0 @@
-import CustomButton from "./CustomButton";
-import AIPicker from "./AIPicker";
-import ColorPicker from "./ColorPicker";
-import FilePicker from "./FilePicker";
-import Tab from "./Tab";
-
-
-
-export {CustomButton, AIPicker,ColorPicker, FilePicker,Tab}
